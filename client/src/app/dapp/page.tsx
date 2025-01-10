97d54ebe9758c7f595fab00e60d26e9d7cdef46b
WangWilly <s89667654@gmail.com>
2025-01-10 17:04:27 -0500

    feat: 🎸 (client) show all doners

--- client/src/app/dapp/page.tsx
+++ client/src/app/dapp/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "@nextui-org/react";
 
 // import log from "@/utils/logging";
 
@@ -10,6 +11,8 @@ import { DataTable } from "@/components/ui/defaultTable";
 import { useAnchorProvider } from "@/components/solana_provider";
 import { getBountyFactoryProgram } from "@/components/anchor/bounty_factory";
 import { Bounty, columns } from "@/components/anchor/dtos/bountyV1";
+import { Donner, columns as donnerColumns } from "@/components/anchor/dtos/donnerV1";
+import log from "@/utils/logging";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -19,14 +22,15 @@ export default function Page() {
   const program = getBountyFactoryProgram(provider);
 
   const [bounties, setBounties] = useState<Bounty[]>([]);
+  const [donners, setDonners] = useState<Donner[]>([]);
 
-  useEffect(() => {
+  const changeOnClick = () => {
     // Resolve
     // https://stackoverflow.com/questions/77742713/how-to-use-async-await-inside-client-component-in-next-js
     // https://stackoverflow.com/questions/77457425/how-to-implement-useeffect-in-the-server-in-next-js-14
     const fetchBounties = async () => {
       const allBounties = await program.account.bountyV1.all([]);
-      // log(allBounties);
+      log(allBounties);
       setBounties(
         allBounties.map(
           (bounty): Bounty => ({
@@ -42,8 +46,24 @@ export default function Page() {
       );
     };
 
+    const fetchDonners = async () => {
+      const allDonners = await program.account.donerV1.all([]);
+      log(allDonners);
+      setDonners(
+        allDonners.map(
+          (donner): Donner => ({
+            doner: donner.publicKey,
+            bounty: donner.account.bounty,
+            donation: donner.account.donation,
+            message: donner.account.message,
+          }),
+        ),
+      );
+    }
+
     fetchBounties();
-  });
+    fetchDonners();
+  };
 
   return (
     <div className="bg-black text-white flex flex-col items-center justify-center min-h-screen">
@@ -76,9 +96,24 @@ export default function Page() {
           </p>
         </div>
         <div className="border border-dotted border-gray-600 p-4 rounded-lg text-center mb-6">
+          <Button
+            onPress={changeOnClick}
+            className="bg-gray-100 hover:bg-gray-300 text-black px-6 py-2 rounded-lg font-semibold"
+          >
+            Refresh
+          </Button>
+        </div>
+        <div className="border border-dotted border-gray-600 p-4 rounded-lg text-center mb-6">
           <DataTable
             columns={columns}
             data={bounties}
+            // className="border border-gray-600 p-4 rounded-lg text-center mb-6"
+          />
+        </div>
+        <div className="border border-dotted border-gray-600 p-4 rounded-lg text-center mb-6">
+          <DataTable
+            columns={donnerColumns}
+            data={donners}
             // className="border border-gray-600 p-4 rounded-lg text-center mb-6"
           />
         </div>
